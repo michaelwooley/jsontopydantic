757c062b28b1fb083336b00766f39dc74034038a
Michael Wooley <william.wooley@motional.com>
2022-10-29 07:21:37 -0400

    check in before delete

--- client/src/components/TranslateOptions.tsx
+++ client/src/components/TranslateOptions.tsx
@@ -1,63 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { type TTranslateOptions, TranslateOptions, } from "./translateOptions";
-import { useForm, SubmitHandler } from "react-hook-form";
-// import { ajvResolver } from '@hookform/resolvers/ajv';
+// import { type TTranslateOptions, TranslateOptions, } from "./translateOptions";
+// import { useForm, SubmitHandler } from "react-hook-form";
+// // import { ajvResolver } from '@hookform/resolvers/ajv';
+// import validator from "@rjsf/validator-ajv8";
+// import Form from "@rjsf/core";
 
 export default function TranslateOptionsComponent() {
 
 
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        ...a
-    } = useForm<TTranslateOptions>({
-        // resolver: ajvResolver(TranslateOptions)
-    });
-    const onSubmit: SubmitHandler<TTranslateOptions> = (data, e) => {
-        e?.preventDefault();
-        console.log(data);
-    };
 
 
     return (
-        <form className="form" onSubmit={handleSubmit(onSubmit)}>
-            {Object.entries(TranslateOptions.properties).map(([k, v]) => {
-                return (<div key={k}>
-                    {k} - {v.type}
-                </div>);
-
-            })}
-            <input type="submit" />
-        </form>
+        // <Form schema={TranslateOptions}
+        //     validator={validator}
+        //     // onChange={console.log}
+        //     onSubmit={console.log}
+        //     onError={console.error} />
+        <>{"None"}</>
     );
 }
 
-type p<T> = {
-    title: string;
-    description: string;
-    default: T,
-    type: "string" | "boolean";
-};
-const BooleanField = ({ name, field }: { name: string, field: p<boolean>; }) => (
-    <div className="field">
+// type p<T> = {
+//     title: string;
+//     description: string;
+//     default: T,
+//     type: "string" | "boolean";
+// };
+// const BooleanField = ({ name, field }: { name: string, field: p<boolean>; }) => (
+//     <div className="field">
 
-        <div className="control">
-            <label className="checkbox">
-                <input type="checkbox" defaultChecked={field.default} />
-                {field.title}
-            </label>
-        </div>
-        <p className="help is-success">{field.description}</p>
-    </div>
-);
+//         <div className="control">
+//             <label className="checkbox">
+//                 <input type="checkbox" defaultChecked={field.default} />
+//                 {field.title}
+//             </label>
+//         </div>
+//         <p className="help is-success">{field.description}</p>
+//     </div>
+// );
 
-const InputField = ({ name, title, description, defaultValue, advanced }: { name: string, title: string; description: string; defaultValue: string; advanced: boolean; }) => (
-    <div className="field">
-        <label className="label" htmlFor={name}>{title}</label>
-        <div className="control">
-            <input className="input" name={name} type="text" placeholder="Text input" defaultValue={defaultValue} />
-        </div>
-        <p className="help">{description}</p>
-    </div>
-);
+// const InputField = ({ name, title, description, defaultValue, advanced }: { name: string, title: string; description: string; defaultValue: string; advanced: boolean; }) => (
+//     <div className="field">
+//         <label className="label" htmlFor={name}>{title}</label>
+//         <div className="control">
+//             <input className="input" name={name} type="text" placeholder="Text input" defaultValue={defaultValue} />
+//         </div>
+//         <p className="help">{description}</p>
+//     </div>
+// );
